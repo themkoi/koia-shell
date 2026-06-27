a5889a41eb19d0f963c2b8ebc16bd408050a56dd
themkoi <themkoia@disroot.org>
2026-06-27 17:24:43 +0200

    fix profiles init

--- src/services/power_profiles/adjuster.rs
+++ src/services/power_profiles/adjuster.rs
@@ -1,4 +1,4 @@
-use log::{info, error};
+use log::{error, info};
 use wayle_power_profiles::PowerProfilesService;
 
 use crate::barWindow;
@@ -9,27 +9,34 @@ pub async fn start_profile_adjuster(
     profile_service: Arc<PowerProfilesService>,
 ) {
     info!("starting power profile adjuster");
-
-    if let Some(ui) = ui_weak.upgrade() {
-        ui.on_set_power_profile(move |profile| {
-            let profile_service = Arc::clone(&profile_service);
-            let profile_str = profile.to_string(); 
-            info!("Received power profile change request from UI: '{}'", profile_str);
-
-            tokio::spawn(async move {
-                let target_profile = wayle_power_profiles::types::profile::PowerProfile::from(
-                    profile_str.as_str()
+    slint::invoke_from_event_loop(move || {
+        if let Some(ui) = ui_weak.upgrade() {
+            ui.on_set_power_profile(move |profile| {
+                let profile_service = Arc::clone(&profile_service);
+                let profile_str = profile.to_string();
+                info!(
+                    "Received power profile change request from UI: '{}'",
+                    profile_str
                 );
 
-                match profile_service
-                    .power_profiles
-                    .set_active_profile(target_profile.clone())
-                    .await 
-                {
-                    Ok(_) => info!("Successfully updated system power profile to {:?}", target_profile),
-                    Err(e) => error!("Failed to set active power profile: {:?}", e),
-                }
+                tokio::spawn(async move {
+                    let target_profile = wayle_power_profiles::types::profile::PowerProfile::from(
+                        profile_str.as_str(),
+                    );
+
+                    match profile_service
+                        .power_profiles
+                        .set_active_profile(target_profile.clone())
+                        .await
+                    {
+                        Ok(_) => info!(
+                            "Successfully updated system power profile to {:?}",
+                            target_profile
+                        ),
+                        Err(e) => error!("Failed to set active power profile: {:?}", e),
+                    }
+                });
             });
-        });
-    }
+        }
+    }).unwrap();
 }
--- src/services/power_profiles/mod.rs
+++ src/services/power_profiles/mod.rs
@@ -9,19 +9,11 @@ use wayle_power_profiles::PowerProfilesService;
 mod adjuster;
 mod listener;
 
-use tokio::sync::oneshot;
-
 pub async fn start_power_profile_management(ui_weak: slint::Weak<barWindow>) {
-    let (tx, rx) = oneshot::channel();
-
     tokio::spawn(async move {
-        let service = PowerProfilesService::new().await.unwrap();
-        tx.send(service).unwrap();
+        let profile = PowerProfilesService::new().await.unwrap();
+
+        listen_profile_changes(ui_weak.clone(), profile.clone()).await;
+        start_profile_adjuster(ui_weak, profile).await;
     });
-
-    let profile = rx.await.unwrap();
-
-    start_profile_adjuster(ui_weak.clone(), profile.clone()).await;
-
-    listen_profile_changes(ui_weak, profile).await;
 }
